594013577541966eaea2aee61acfac93b3d59f96
anaselhajjaji <elhajjaji.anas@gmail.com>
2016-01-18 16:30:00 +0000

    Call GC to accelerate the analysis

--- LeakCanarySample/app/src/main/java/anaware/leakcanarysample/AnotherActivity.java
+++ LeakCanarySample/app/src/main/java/anaware/leakcanarysample/AnotherActivity.java
@@ -11,6 +11,9 @@ public class AnotherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another);
+
+        // Invoke the GC to accelerate the analysis.
+        System.gc();
     }
 
     @Override
