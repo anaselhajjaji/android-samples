1712e856411e68f4993427559451e40b10948d4c
anaselhajjaji <elhajjaji.anas@gmail.com>
2016-03-26 22:43:37 +0000

    Create RxAndroid sample application

---
+++ RxAndroidSample/app/src/main/java/anaware/samples/rxandroid/JsonAdapter.java
@@ -0,0 +1,81 @@
+package anaware.samples.rxandroid;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class JsonAdapter extends android.support.v7.widget.RecyclerView.Adapter {
+    private List<Song> songs;
+
+    public JsonAdapter(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new JsonViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Song song = songs.get(position);
+        ((JsonViewHolder) holder).bindViewHolder(song);
+    }
+
+    @Override
+    public int getItemCount() {
+        return songs.size();
+    }
+
+    public class JsonViewHolder extends android.support.v7.widget.RecyclerView.ViewHolder {
+
+        @Bind(R.id.artistTv)
+        TextView artistTv;
+
+        @Bind(R.id.timesTv)
+        TextView timeTv;
+
+        @Bind(R.id.trackIdTv)
+        TextView trackIdTv;
+
+        @Bind(R.id.titleTv)
+        TextView titleTv;
+
+        @Bind(R.id.songImage)
+        ImageView songImage;
+
+        public JsonViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+        public void bindViewHolder(Song song) {
+            artistTv.setText(song.getArtist());
+            timeTv.setText(song.getSongDate().toString());
+            trackIdTv.setText(song.getTrackId());
+            titleTv.setText(song.getTitle());
+
+            // Download image
+            Picasso.with(titleTv.getContext())
+                    .load(song.getTrackImage())
+                    .placeholder(android.R.drawable.ic_menu_rotate)
+                    .error(android.R.drawable.ic_menu_camera)
+                    .into(songImage);
+        }
+    }
+}
+
+
+
+
---
+++ RxAndroidSample/app/src/main/java/anaware/samples/rxandroid/MainActivity.java
@@ -0,0 +1,159 @@
+package anaware.samples.rxandroid;
+
+import android.app.ProgressDialog;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.HttpGet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.client.methods.CloseableHttpResponse;
+import cz.msebera.android.httpclient.impl.client.CloseableHttpClient;
+import cz.msebera.android.httpclient.impl.client.HttpClientBuilder;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.android.schedulers.HandlerScheduler;
+import rx.functions.Func0;
+
+import static android.os.Process.THREAD_PRIORITY_BACKGROUND;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
+    private android.support.v7.widget.RecyclerView.LayoutManager layoutManager;
+    private ProgressDialog progressDialog;
+
+    // Background worker
+    private Handler backgroundHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Start the background task
+        BackgroundThread backgroundThread = new BackgroundThread();
+        backgroundThread.start();
+        backgroundHandler = new Handler(backgroundThread.getLooper());
+
+        // Initialize the recycler view
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Show progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage(getString(R.string.loading_msg));
+        progressDialog.show();
+
+        // Fetch songs
+        String url = getString(R.string.data_url_github);
+        fetchSongs(url);
+    }
+
+    void fetchSongs(String url) {
+        songsObservable(url)
+                // Run on a background thread
+                .subscribeOn(HandlerScheduler.from(backgroundHandler))
+                        // Be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<Song>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(getClass().getSimpleName(), "onCompleted()");
+                        progressDialog.dismiss();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(getClass().getSimpleName(), "onError()", e);
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onNext(List<Song> songs) {
+                        recyclerView.setAdapter(new JsonAdapter(songs));
+                    }
+                });
+    }
+
+    static Observable<List<Song>> songsObservable(final String url) {
+        return Observable.defer(new Func0<Observable<List<Song>>>() {
+            @Override public Observable<List<Song>> call() {
+                CloseableHttpClient httpClient = null;
+                try  {
+                    httpClient = HttpClientBuilder.create().build();
+
+                    // Prepare a request object
+                    HttpGet httpget = new HttpGet(url);
+
+                    CloseableHttpResponse response = null;
+                    try {
+                        response = httpClient.execute(httpget);
+                        InputStream responseStream = response.getEntity().getContent();
+                        return Observable.just(parseJson(responseStream));
+                    }
+                    catch(JsonIOException e1) {
+                        return Observable.error(e1);
+                    }
+                    catch(JsonSyntaxException e2) {
+                        return Observable.error(e2);
+                    }
+                    finally {
+                        if (response != null) {
+                            response.close();
+                        }
+                    }
+                } catch (IOException e) {
+                    return Observable.error(e);
+                }
+                finally {
+                    if (httpClient != null) {
+                        try {
+                            httpClient.close();
+                        } catch (IOException e) {
+                            return Observable.error(e);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private static List<Song> parseJson(InputStream rawJsonStream) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat("yyyy-MM-dd'T'hh:MM:ss");
+        Gson gson = gsonBuilder.create();
+        Type collectionType = new TypeToken<List<Song>>() {
+        }.getType();
+        List<Song> songs = gson.fromJson(new InputStreamReader(rawJsonStream), collectionType);
+        return songs;
+    }
+
+    static class BackgroundThread extends HandlerThread {
+        BackgroundThread() {
+            super("SchedulerSample-BackgroundThread", THREAD_PRIORITY_BACKGROUND);
+        }
+    }
+}
